999fe72835c5f556f5e2353711c52996ba3c459a
Alison Maziero <alison_lens@hotmail.com>
2021-05-09 15:24:07 -0300

    feat: new create account route

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
 
 const app = express();
+const custumers = [];
 
-app.get('', (request, response) => {});
+app.post('/account', (request, response) => {
+	const { cpf, name } = request.body;
+	const id = uuidv4();
+
+	custumers.push({
+		cpf,
+		name,
+		id,
+		statement: []
+	})
+
+	return response.status(201).send('Salvo com sucesso')
+});
 
 app.listen('3333', () => {
 	console.log('Server starter')
