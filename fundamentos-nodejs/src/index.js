7fd679ddbbc13dcfb013ab1fc06d41bfce1325d8
Alison Maziero <alison_lens@hotmail.com>
2021-05-09 15:39:04 -0300

    feat: validated if the cpf alaready are registered

--- fundamentos-nodejs/src/index.js
+++ fundamentos-nodejs/src/index.js
@@ -8,12 +8,19 @@ const custumers = [];
 
 app.post('/account', (request, response) => {
 	const { cpf, name } = request.body;
-	const id = uuidv4();
+
+	const custumerAlreadyExists = custumers.some(
+		(custumer) => custumer.cpf === cpf
+	);
+
+	if(custumerAlreadyExists) {
+		return response.status(400).json({ message: 'Custumer already exists' })
+	}
 
 	custumers.push({
 		cpf,
 		name,
-		id,
+		id: uuidv4(),
 		statement: []
 	})
 
