05b7978a2917015e29b2973f8f14fa2b86e5d569
Alison Maziero <alison_lens@hotmail.com>
2021-05-12 19:44:51 -0300

    refactor: fix route params

--- typescript/src/routes.ts
+++ typescript/src/routes.ts
@@ -2,7 +2,11 @@ import { Request, Response } from 'express';
 import CreateCoursesService from './CreateCoursesService ';
 
 export function createCourse(request: Request, response: Response) {
-	CreateCoursesService.execulte("nodejs", 10, "eu mesmo");
+	CreateCoursesService.execulte({
+		name: "nodejs",
+		duration: 10,
+		educator: "eu mesmo"
+	});
 
 	return response.send();
 };
