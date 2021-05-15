b82361d351c57fbd128af1d5d669bd9bab6c9255
Alison Maziero <alison_lens@hotmail.com>
2021-05-15 20:22:48 -0300

    feat: apply liskov principle into reporisory

---
+++ rentalx/src/repositories/ICategoryRepository.ts
@@ -0,0 +1,10 @@
+import { ICreateCategoryDTO } from "../dtos/ICategoriesDTO";
+import { Category } from "../model/Category";
+
+interface ICategoriesRepository {
+  findByName(name: string): Category | undefined;
+  list(): Category[];
+  create({ name, description }: ICreateCategoryDTO): void;
+}
+
+export { ICategoriesRepository };
