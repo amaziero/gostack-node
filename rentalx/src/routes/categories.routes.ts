79d3b284d88b8b0d62918cc0c5e6aa99ad8efa6b
Alison Maziero <alison_lens@hotmail.com>
2021-05-16 14:03:09 -0300

    feat: create index exporting dependencies and fix route

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -1,6 +1,7 @@
 import { Router } from "express";
 
-import { createCategoryController } from "../modules/cars/useCases/createCategory/index";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategory";
 
 const categoriesRoutes = Router();
 
@@ -8,6 +9,8 @@ categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
 });
 
-categoriesRoutes.get("/", (request, response) => {});
+categoriesRoutes.get("/", (request, response) => {
+  return listCategoriesController.handle(request, response);
+});
 
 export { categoriesRoutes };
