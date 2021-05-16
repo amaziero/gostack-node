12de8d9e77065c0befcf54897f0e5e9d6da0912c
Alison Maziero <alison_lens@hotmail.com>
2021-05-16 11:15:21 -0300

    fix: fix methods not initialized

--- rentalx/src/model/Category.ts
+++ rentalx/src/model/Category.ts
@@ -1,16 +1,16 @@
-import { v4 as uuid } from 'uuid';
+import { v4 as uuid } from "uuid";
 
 class Category {
-	id?: string;
-	name: string;
-	description: string;
-	created_at: Date;
+  id?: string;
+  name: string | undefined;
+  description: string | undefined;
+  created_at: Date | undefined;
 
-	constructor() {
-		if(!this.id) {
-			this.id = uuid()
-		}
-	};
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
 }
 
-export { Category }
+export { Category };
