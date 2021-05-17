40035bcf87f58582f207858fb1672b87ebfef16a
Alison Maziero <alison_lens@hotmail.com>
2021-05-17 20:38:16 -0300

    feat: started working with multer

--- rentalx/src/routes/categories.routes.ts
+++ rentalx/src/routes/categories.routes.ts
@@ -3,7 +3,12 @@ import { Router } from "express";
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
 import { listCategoriesController } from "../modules/cars/useCases/listCategory";
 
+import multer from "multer";
+
 const categoriesRoutes = Router();
+const upload = multer({
+  dest: "./temp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -12,5 +17,11 @@ categoriesRoutes.post("/", (request, response) => {
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
 });
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+  
+  return response.send();
+})
 
 export { categoriesRoutes };
