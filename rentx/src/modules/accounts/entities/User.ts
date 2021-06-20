b911749d83c0a885413506bea9140ec8742d0dd6
Alison Maziero <alison_lens@hotmail.com>
2021-06-20 17:54:18 -0300

    feat: users migration and entities

---
+++ rentx/src/modules/accounts/entities/User.ts
@@ -0,0 +1,49 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+
+@Entity("users")
+class Users {
+  @PrimaryColumn()
+  id: string | undefined;
+
+  @Column()
+  name: string | undefined;
+
+  @Column()
+  username: string | undefined;
+
+  @Column()
+  password: string | undefined;
+
+  @Column()
+  email: string | undefined;
+
+  @Column()
+  driver_license: string | undefined;
+
+  @Column()
+  isAdmin: boolean | undefined;
+
+  @Column()
+  description: string | undefined;
+
+  @CreateDateColumn()
+  created_at: Date | undefined;
+
+  @UpdateDateColumn()
+  updated_at: Date | undefined;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Users };
