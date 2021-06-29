771809532721b2e44902dab9415d83dbb9321c48
Alison Maziero <alison_lens@hotmail.com>
2021-06-28 21:40:31 -0300

    fix: ajust id and avatar at creation

--- rentx/src/routes/user.routes.ts
+++ rentx/src/routes/user.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import multer from "multer";
 
 import uploadConfig from "../config/upload";
+import { ensuereAuthenticate } from "../middleware/ensureAuthenticated";
 import { CreateUserController } from "../modules/accounts/useCases/createUser/CreateUserController";
 import { UpdateUserAvatarController } from "../modules/accounts/useCases/updateUserAvatar/UpdateUserAvatarController";
 
@@ -15,6 +16,7 @@ usersRoutes.post("/", createUserController.handle);
 
 usersRoutes.patch(
   "/avatar",
+  ensuereAuthenticate,
   uploadAvatar.single("avatar"),
   updatateUserAvatarController.handle
 );
